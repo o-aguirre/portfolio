1fb596b519a3cefe631bbe0e5f755b6bfb4540d6
o-aguirre <two.aguirre@gmail.com>
2025-11-13 23:46:17 -0300

    add hero

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Hero from './components/hero/Hero';
+
+const App = () => {
+    return(
+        <main className='bg-[#0d182e]'>
+            <Hero />
+        </main>
+    )
+}
+export default App;
